2e99cc48a39388b4b9a8a609d1dd8274b3dc2e13
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-11-05 01:03:26 -0600

    all project endpoints working

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
-const resourceRouter = require('./resource/router')
+const resourceRouter = require('./resource/router');
+const projectRouter = require('./project/router');
 
 const server = express();
 
 
 server.use(express.json());
 server.use('/api/resources', resourceRouter);
+server.use('/api/projects', projectRouter)
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(500).json({
